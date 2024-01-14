5cee6501a04f34b20ee465ce2403bb43e7b10737
Dana <danadev94@gmail.com>
2024-01-14 15:43:24 +0100

    Fix merge issues

--- src/pages/player_chosen.py
+++ src/pages/player_chosen.py
@@ -53,13 +53,16 @@ def getFilteredDF(filters):
     return sourceDF[sourceDF['in_bound'] == "YES"]
 
 
-
 filePath = os.path.join(os.path.dirname(__file__), '../data/player_gca.csv')
 df_defense = pd.read_csv(filePath, encoding='utf-8')
 playersList = [(index, player['player']) for index, player in df_defense.iterrows()]
 
 dash.register_page(__name__, path_template='/replace/<player_id>')
 
+
+def layout():
+    return html.Div([dcc.Store('chosen_player', data=player, storage_type='local'), dcc.Store('filters', data={'position': player['position']}, storage_type='local'), html.Header([]),
+        html.Section([html.Aside([html.Span('chevron_left', className='close-aside material-symbols-rounded'), filters.layout(sourceDF, player), html.Div('hi', id='testing')], id='aside'), specific_players.specific_plots_component(player)])], id='general_page')
 
 
 def layout(player_id=None):
@@ -71,65 +74,32 @@ def layout(player_id=None):
     image_path = get_first_vertical_image(path)
 
     if image_path:
-        return html.Div(
-            id='player-image-container',
-            className='player-chosen-container',
-            children=[
-                # Left half (player's image, name, and team flag)
-                html.Div(
-                    className='half',
-                    children=[
-                        # Player's image
-                        html.Div([html.Img(src=dash.get_asset_url(image_path)), ], className='player-image'),
-                        # Player's name
-                        html.Div(f"Player: {player['player']}", className='player-name'),
-                        # Team flag
-                        html.Div([
-                            html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))),
-                        ], className='team-flag'),
-                    ]),
+        playerImageBox = html.Div(id='player-image-container', className='player-chosen-container', children=[html.Div(className='half', children=[  # Player's image
+                html.Div([html.Img(src=dash.get_asset_url(image_path)), ], className='player-image'), html.Div(f"Player: {player['player']}", className='player-name'),  # Team flag
+                html.Div([html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))), ], className='team-flag'), ]),
 
                 # Separating bar
                 html.Div(className='separating-bar'),
 
                 # Right half (question mark and search bar)
-                html.Div(
-                    id='unknown-player-right',
-                    className='half',
-                    children=[
-                        # Question mark image
-                        html.Div([
-                            html.Img(src=dash.get_asset_url('icons/unknown_user_right.svg'))], className='player-image'),
+                html.Div(id='unknown-player-right', className='half', children=[  # Question mark image
+                    html.Div([html.Img(src=dash.get_asset_url('icons/unknown_user_right.svg'))], className='player-image'),
 
-                        # Search bar
-                        dcc.Dropdown(
-                            id='select_player_name_chosen',
-                            options=[{'label': playerItem[1], 'value': playerItem[0]} for playerItem in playersList],
-                            placeholder="Search for a player...",
-                        ),
-                    ]
-                ),
+                    # Search bar
+                    dcc.Dropdown(id='select_player_name_chosen', options=[{'label': playerItem[1], 'value': playerItem[0]} for playerItem in playersList], placeholder="Search for a player...", ), ]),
 
                 # Selected player information outside of 'unknown-player-right'
-                html.Div(
-                    id='selected-player-info',
-                    className='half',
-                ),
-            ]
-        )
+                html.Div(id='selected-player-info', className='half', ), ])
     else:
-        # Handle the case when the image is not found
         return html.Div("Image not found")
+    return html.Div([dcc.Store('chosen_player', data=player, storage_type='local'), dcc.Store('filters', data={'position': player['position']}, storage_type='local'), html.Header([]),
+        html.Section([html.Aside([playerImageBox,html.Span('chevron_left', className='close-aside material-symbols-rounded'), filters.layout(sourceDF, player), html.Div('hi', id='testing')], id='aside'), specific_players.specific_plots_component(player)])], id='general_page')
 
 
 # -------------------------------------------------------------
 # Callbacks to update player box based on chosen players: Akseniia
 # -------------------------------------------------------------
-@callback(
-    [Output('selected-player-info', 'children'),
-     Output('unknown-player-right', 'style')],
-    [Input('select_player_name_chosen', 'value')]
-)
+@callback([Output('selected-player-info', 'children'), Output('unknown-player-right', 'style')], [Input('select_player_name_chosen', 'value')])
 def update_selected_player_info(player_id):
     if player_id:
         player_id = int(player_id)
@@ -139,19 +109,14 @@ def update_selected_player_info(player_id):
 
         if image_path:
             # Return the selected player information and hide the 'unknown-player-right'
-            return [
-                [
-                    html.Div([html.Img(src=dash.get_asset_url(image_path)),], className='player-image'),
-                    html.Div(f"Player: {player['player']}", className='player-name'),
-                    html.Div([html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))),], className='team-flag'),
-                ],
-                {'display': 'none'},  # Hide the 'unknown-player-right'
+            return [[html.Div([html.Img(src=dash.get_asset_url(image_path)), ], className='player-image'), html.Div(f"Player: {player['player']}", className='player-name'), html.Div([html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))), ], className='team-flag'), ], {'display': 'none'},
+                # Hide the 'unknown-player-right'
             ]
 
         print('No `player_id` passed...')
         return ""  # TODO: handle this properly
     player = sourceDF.iloc[[player_id]].to_dict(orient='records')[0]
-    return html.Div([ dcc.Store('chosen_player', data=player, storage_type='local'), dcc.Store('filters', data={'position': player['position']}, storage_type='local'), html.Header([]),
+    return html.Div([dcc.Store('chosen_player', data=player, storage_type='local'), dcc.Store('filters', data={'position': player['position']}, storage_type='local'), html.Header([]),
         html.Section([html.Aside([html.Span('chevron_left', className='close-aside material-symbols-rounded'), filters.layout(sourceDF, player), html.Div('hi', id='testing')], id='aside'), specific_players.specific_plots_component(player)])], id='general_page')
 
 
