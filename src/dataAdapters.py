c17b873f58a8086bd98710868fea2c5dfa629c51
20221863 <a.konashenkova@student.tue.nl>
2024-01-14 15:05:17 +0100

    Implement chosen player box

--- src/dataAdapters.py
+++ src/dataAdapters.py
@@ -1,17 +1,27 @@
 import pandas as pd
-import os
 import pycountry
+from typing import Union
+import os
+from PIL import Image
+import re
+import dash
+import urllib.parse
+import unicodedata
+from pathlib import Path
 
 
-def getTeamGroup(team: str, mapToLetters=False) -> int | str:
+
+
+def getTeamGroup(team: str, mapToLetters=False) -> Union[int, str]:
     """
     :type team: int | str <- The input team (either in the group number of the corresponding letter in uppercase)
     :rtype: int <- The letter representing the group the team belongs to | returns 0 if the team group could not be found
     """
     team = team.capitalize()
+    print('team: ', team)
     try:
-        group_stats_csv_path = os.path.join(os.path.dirname(__file__), 'data/group_stats.csv')
-        df_groups = pd.read_csv(group_stats_csv_path)
+        group_stats_csv_path = os.path.join(os.path.dirname(__file__), r"C:\uni\Y2Q2\Visualization\proj\notebooks\data\group_stats.csv")
+        df_groups = pd.read_csv(group_stats_csv_path, encoding='utf-8')
 
         filtered = df_groups[df_groups.team == team]
         groupNumber = int(filtered.group.unique()[0])
@@ -22,31 +32,107 @@ def getTeamGroup(team: str, mapToLetters=False) -> int | str:
         return 0
 
 
-def getPlayerTeam(playerName: str) -> str | bool:
+def getPlayerTeam(playerName: str) -> Union[str, bool]:
     """
     :rtype: str | bool <- The name of player's team in the games | returns False in case the player couldn't be found in the source dataset (players_misc.csv)
     :type playerName: str <- The name of the player to retrieve the team for
     """
     try:
         players_csv_path = os.path.join(os.path.dirname(__file__), 'data/player_misc.csv')
-        df_players = pd.read_csv(players_csv_path)
+        df_players = pd.read_csv(players_csv_path, encoding='utf-8')
         filtered = df_players[df_players.player == playerName]
         return (filtered.team.unique()[0])
     except:
         return False
 
 
+def natural_sort_key(s):
+    """
+    Generate a key for natural sorting based on numeric values in a string.
+
+    :param s: The input string
+    :type s: str
+    :return: A list of comparable items, including numeric parts
+    :rtype: list
+    """
+    return [int(text) if text.isdigit() else text.lower() for text in re.split('([0-9]+)', s)]
+
+
+def get_first_vertical_image(directory_path):
+    """
+    Find and return the filename of the first vertical image in the specified directory.
+
+    :param directory_path: The path to the directory containing player_images
+    :type directory_path: str
+    :return: The filename of the first vertical image, or None if not found
+    :rtype: str | None
+    """
+    try:
+        if os.path.isdir(directory_path):
+            filenames = sorted(os.listdir(directory_path), key=natural_sort_key)
+
+            for filename in filenames:
+                if filename.endswith(('.jpg', '.jpeg', '.png')):
+                    image_path = os.path.join(directory_path, filename)
+                    try:
+                        img = Image.open(image_path)
+                        width, height = img.size
+
+                        # Check if the image is vertically oriented
+                        if height > width:
+                            encoded_filename = filename.encode('unicode-escape').decode('utf-8')
+                            image_path = os.path.join(directory_path.split('assets/', 1)[1], encoded_filename)
+                            return image_path
+                    except Exception as e:
+                        print(f"Error processing image {filename}: {e}")
+            return None
+        else:
+            # If the path is a file, return it directly
+            return directory_path
+    except Exception as e:
+        print(f"Error processing directory {directory_path}: {e}")
+        return None
+
+
+
 def playerImageDirectory(playerName, playerTeam=None, playerGroup=None):
-    if not playerTeam:
-        # In case the name of the player team is not provided, `getPlayerTeam()` is used to look it up
-        playerTeam = getPlayerTeam(playerName)
-    if not playerGroup:
-        # In case the name of the player team group is not provided, `playerTeam()` is used to look it up
-        playerGroup = getTeamGroup(playerTeam, mapToLetters=True)
+    """
+    Generate the directory path for a player's player_images.
 
-    # TODO: handle exceptions <- IR Iran for example
+    :param playerName: The name of the player
+    :type playerName: str
+    :param playerTeam: The name of the player's team
+    :type playerTeam: str
+    :param playerGroup: The name of the player's team group
+    :type playerGroup: str
+    :return: The directory path for the player's player_images
+    :rtype: str
+    """
+    try:
+        if not playerTeam:
+            # In case the name of the player team is not provided, getPlayerTeam() is used to look it up
+            playerTeam = getPlayerTeam(playerName)
+        if not playerGroup:
+            print('group: ', playerGroup)
+            # In case the name of the player team group is not provided, playerTeam() is used to look it up
+            playerGroup = getTeamGroup(playerTeam, mapToLetters=True)
 
-    return "GROUP {}/{} Players/Images_{}".format(playerGroup, playerTeam, playerName)
+        # Handle exceptions here if needed
+        # TODO: handle exceptions <- IR Iran for example
+
+        print(f"Player: {playerName}, Team: {playerTeam}, Group: {playerGroup}")
+
+        # formatted_player_name = playerName.encode('unicode-escape').decode('utf-8')
+        directory_path = f"assets/player_images/Group {playerGroup}/{playerTeam} Players/Images_{playerName}"
+
+        # Check if the directory exists, otherwise display 'unknown_user2.png'
+        if os.path.exists(directory_path):
+            return directory_path
+        else:
+            return "icons/unknown_user_left.svg"
+    except Exception as e:
+        print(f"Error generating player image directory: {e}")
+        return "icons/unknown_user_left.svg"
 
 
 def getCountryFlagPath(countryName: str):
--- src/pages/player_chosen.py
+++ src/pages/player_chosen.py
@@ -1,23 +1,98 @@
 import dash
-from dash import Dash, html, dcc, callback, Output, Input, dash_table
+from dash import html, dcc, callback, Output, Input
 import os
 import pandas as pd
+from dataAdapters import getCountryFlagPath, playerImageDirectory, getPlayerTeam, get_first_vertical_image, getTeamGroup
+
 
 dash.register_page(__name__, path_template='/replace/<player_id>')
 
-# TODO: the following is duplicate. Outsource it.
 filePath = os.path.join(os.path.dirname(__file__), '../data/player_gca.csv')
-df_defense = pd.read_csv(filePath)
+df_defense = pd.read_csv(filePath, encoding='utf-8')
+playersList = [(index, player['player']) for index, player in df_defense.iterrows()]
 
 
 def layout(player_id=None):
     if not player_id:
-        print('No `player_id` passed...')
         return ""
-        # TODO: handle this properly
-    player = df_defense.iloc[[player_id]].to_dict(orient='records')[0]
 
-    return [
-        html.Div(
-            'Hi and welcome. You have chosen player with id {} That would be {}'.format(player_id, player['player'])),
-    ]
+    player = df_defense.iloc[[player_id]].to_dict(orient='records')[0]
+    path = playerImageDirectory(player['player'])
+    image_path = get_first_vertical_image(path)
+
+    if image_path:
+        return html.Div(
+            id='player-image-container',
+            className='player-chosen-container',
+            children=[
+                # Left half (player's image, name, and team flag)
+                html.Div(
+                    className='half',
+                    children=[
+                        # Player's image
+                        html.Div([html.Img(src=dash.get_asset_url(image_path)), ], className='player-image'),
+                        # Player's name
+                        html.Div(f"Player: {player['player']}", className='player-name'),
+                        # Team flag
+                        html.Div([
+                            html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))),
+                        ], className='team-flag'),
+                    ]),
+
+                # Separating bar
+                html.Div(className='separating-bar'),
+
+                # Right half (question mark and search bar)
+                html.Div(
+                    id='unknown-player-right',
+                    className='half',
+                    children=[
+                        # Question mark image
+                        html.Div([
+                            html.Img(src=dash.get_asset_url('icons/unknown_user_right.svg'))], className='player-image'),
+
+                        # Search bar
+                        dcc.Dropdown(
+                            id='select_player_name_chosen',
+                            options=[{'label': playerItem[1], 'value': playerItem[0]} for playerItem in playersList],
+                            placeholder="Search for a player...",
+                        ),
+                    ]
+                ),
+
+                # Selected player information outside of 'unknown-player-right'
+                html.Div(
+                    id='selected-player-info',
+                    className='half',
+                ),
+            ]
+        )
+    else:
+        # Handle the case when the image is not found
+        return html.Div("Image not found")
+
+
+# Callback to update content based on player ID
+@callback(
+    [Output('selected-player-info', 'children'),
+     Output('unknown-player-right', 'style')],
+    [Input('select_player_name_chosen', 'value')]
+)
+def update_selected_player_info(player_id):
+    if player_id:
+        player_id = int(player_id)
+        player = df_defense.iloc[[player_id]].to_dict(orient='records')[0]
+        path = playerImageDirectory(player['player'])
+        image_path = get_first_vertical_image(path)
+
+        if image_path:
+            # Return the selected player information and hide the 'unknown-player-right'
+            return [
+                [
+                    html.Div([html.Img(src=dash.get_asset_url(image_path)),], className='player-image'),
+                    html.Div(f"Player: {player['player']}", className='player-name'),
+                    html.Div([html.Img(src=dash.get_asset_url(getCountryFlagPath(getPlayerTeam(player['player'])))),], className='team-flag'),
+                ],
+                {'display': 'none'},  # Hide the 'unknown-player-right'
+            ]
+
