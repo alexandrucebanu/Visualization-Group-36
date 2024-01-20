62869f117a6781fa2133e8743c3bb2a3e66bbf0d
larsenalicia <alicia.hh.larsen@gmail.com>
2024-01-20 20:14:27 +0100

    radar chart with tabs

--- src/pages/bookmarks.py
+++ src/pages/bookmarks.py
@@ -6,12 +6,58 @@ from dash import Input, Output
 from dash import dcc
 import plotly.graph_objects as go
 from .helper_functions import import_data
+from sklearn.preprocessing import StandardScaler 
+import pandas as pd
+
 
 dash.register_page(__name__, path_template='/bookmarks')
 
-sourceDF = import_data.importData()
 
-chosen_players = [456, 362, 22]
+# Dummy players
+#chosen_players = [482, 388, 134] # Neymar, messi, Ronaldo
+chosen_players = [4, 83, 410] # Neymar, messi, Ronaldo
+
+sourceDF = import_data.importData()
+df_standardised = pd.DataFrame()
+
+# Statistics categories
+categories_stats = ['games', 'minutes_90s', 'cards_red', 'cards_yellow']
+titles_stats = ['Number of games', 'Minutes Player / 90', 'Red Cards', 'Yellow Cards']
+
+# Passing categories
+categories_passing = ['passes_pct', 'passes_progressive_distance', 'passes_pct_short', 'passes_pct_medium', 'passes_pct_long']
+titles_passing = ['Percentage Completed Passes', 'Total Progressive Distance', 'Percentage of Completed Short-Distance Passes', 'Percentage of Completed Medium-Distance Passes', 'Percentage of Completed Long-Distance Passes']
+
+# Set Piece
+categories_set_piece = ['throw_ins', 'corner_kicks_in', 'corner_kicks_out', 'corner_kicks_straight', 'corner_kicks']
+titles_set_piece = ['Number of Throw-Ins', 'Number of Corner Kicks', 'Number of Corner Kicks type "In"', 'Number of Corner Kicks type "Out"', 'Number of Corner Kicks type "Straight"']
+
+# GCA categories
+categories_gca = ['sca', 'sca_per90', 'gca', 'gca_per90']
+titles_gca = ['Shot-Creating Actions', 'Shot-Creating Actions per 90 min', 'Goal-Creating Actions', 'Goal-Creating Actions per 90 min']
+
+# Shooting categories
+categories_shooting = ['shots_per90','shots_on_target_per90','goals_per_shot_on_target',
+              'average_shot_distance', 'shots_free_kicks', 'pens_made']
+titles_shooting = ['Shots per 90','Shots on Target per 90','Goals per Shot on Target',
+              'Average Shot Distance', 'Shots Free Kicks', 'Penalties made']
+
+# Defense categories
+categories_defense = ['tackles', 'tackles_won', 'dribble_tackles_pct', 'blocked_shots', 'blocked_passes']
+titles_defense = ['Number of Tackles', 'Number of Tackles Won', 'Percentage of Tackles which are type "Dribble"', 'Percentage of Tackles which are type "Shots"', 'Percentage of Tackles which are type "Passes"']
+
+categories = categories_stats + categories_passing + categories_set_piece + categories_gca + categories_shooting + categories_defense
+titles = titles_stats + titles_passing + titles_set_piece + titles_gca + titles_shooting + titles_defense
+
+
+for col in categories:
+    df_standardised[col] = (sourceDF[col] - sourceDF[col].mean()) / sourceDF[col].std() 
+
+df_standardised.rename(columns={categories[i]: titles[i] for i in range(len(categories))}, inplace=True)
+
+min_vals = df_standardised.min()
+df_standardised[titles] = df_standardised[titles] - min_vals
+
 
 def layout():
     return html.Div(id='bookmarks_page', children=[
@@ -79,57 +125,49 @@ def addTabs():
     html.Div(id='tabs-content-classes')
     ])
 
+
+def makeRadar(titles):
+    fig = go.Figure()
+
+    for player_ID in chosen_players:
+        
+        fig.add_trace(go.Scatterpolar(
+            theta=titles,
+            r=[df_standardised.loc[player_ID, var] for var in titles],
+            fill='toself',
+            name=sourceDF.loc[player_ID, 'player']
+        ))
+    
+    fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+        visible=True,
+        showline=True,
+        showticklabels=False,
+        showgrid=True,
+        autorange = True,
+        )),
+    showlegend=True
+    )
+    fig.update_layout(margin={'l': 400, 't': 40, 'b': 0, 'r': 0})
+    
+    return html.Div([
+        dcc.Graph(figure=fig, config={'staticPlot': True})
+    ])
+
+
 @callback(Output('tabs-content-classes', 'children'),
               Input('tabs-with-classes', 'value'))
 def render_content(tab):
     if tab == 'tab-stats':
-        return html.Div([
-            html.H3('Tab content 1')
-        ])
+        return makeRadar(titles_stats)
     elif tab == 'tab-passing':
-        return html.Div([
-            html.H3('Tab content 2')
-        ])
+        return makeRadar(titles_passing)
     elif tab == 'tab-set-piece':
-        return html.Div([
-            html.H3('Tab content 3')
-        ])
+        return makeRadar(titles_set_piece)
     elif tab == 'tab-gca':
-        return html.Div([
-            html.H3('Tab content 4')
-        ])
+        return makeRadar(titles_gca)
     elif tab == 'tab-shooting':
-        
-        categories = ['shots_per90','shots_on_target_per90','goals_per_shot_on_target',
-              'average_shot_distance', 'shots_free_kicks', 'pens_made']
-
-        fig = go.Figure()
-
-        for player_ID in chosen_players:
-            
-            fig.add_trace(go.Scatterpolar(
-                theta=categories,
-                r=[sourceDF[var] for var in categories],
-                fill='toself',
-                name='Product A'
-            ))
-        
-        fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-            visible=True,
-            #range=[0, 5]
-            )),
-        showlegend=False
-        )
-
-        fig.update_layout(showlegend=True)
-        fig.update_layout(margin={'l': 400, 't': 40, 'b': 0, 'r': 0})
-        
-        return html.Div([
-            dcc.Graph(figure=fig, config={'staticPlot': True})
-        ])
+        return makeRadar(titles_shooting)
     elif tab == 'tab-defense':
-        return html.Div([
-            html.H3('Tab content 6')
-        ])
+        return makeRadar(titles_defense)
